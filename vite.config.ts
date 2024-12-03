44ebce7e6869b2d39faded42142430f42eedfd3f
sim <>
2024-12-03 20:30:01 +0100

    change base name

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/kate-app/',
+    base: '/sim-commits/kate-app/',
     plugins: [react()],
 });
