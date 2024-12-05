93019cb8253da985c91dd2aeeb71b06e88e3ae5c
sim <>
2024-12-05 23:29:04 +0100

    rearrange images

--- src/components/Triptych.tsx
+++ src/components/Triptych.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import imageLeft from '../assets/tempImageqr86uX.png';
-import imageCentral from '../assets/tempImageXXMapy.png';
-import imageRight from '../assets/tempImagePoSIlC.png';
+import image1 from '../assets/tempImageqr86uX.png';
+import image2 from '../assets/tempImageXXMapy.png';
+import image3 from '../assets/tempImagePoSIlC.png';
 
 const Triptych: React.FC = () => {
     return (
         <div className='flex flex-col h-[calc(100vh-104px)] md:flex-row md:h-[calc(100vh-192px)] overflow-hidden'>
             {/* First Image */}
-            <div className='flex-1 relative group'>
-                <img src={imageLeft} alt='Image 1' className='absolute inset-0 w-full h-full object-cover object-[center_10%] md:object-[center_20%] transition-transform duration-500 ease-in-out group-hover:scale-110' />
+            <div className='flex-grow relative group'>
+                <img src={image3} alt='Image 1' className='absolute inset-0 w-full h-full object-cover object-[center_20%] md:object-[center_20%] transition-transform duration-500 ease-in-out group-hover:scale-110' />
             </div>
             {/* Second Image */}
-            <div className='flex-1 relative group'>
-                <img src={imageCentral} alt='Image 2' className='absolute inset-0 w-full h-full object-cover object-[center_10%] md:object-[center_top] transition-transform duration-500 ease-in-out group-hover:scale-110' />
+            <div className='flex-grow relative group'>
+                <img src={image2} alt='Image 2' className='absolute inset-0 w-full h-full object-cover object-[center_10%] md:object-[center_10%] transition-transform duration-500 ease-in-out group-hover:scale-110' />
             </div>
             {/* Third Image */}
-            <div className='flex-1 relative group'>
-                <img src={imageRight} alt='Image 3' className='absolute inset-0 w-full h-full object-cover object-[center_20%] scale-80 transition-transform duration-500 ease-in-out group-hover:scale-110' />
+            <div className='flex-grow relative group'>
+                <img src={image1} alt='Image 3' className='absolute inset-0 w-full h-full object-cover object-[center_10%] md:object-[center_top] transition-transform duration-500 ease-in-out group-hover:scale-110' />
             </div>
         </div>
     );
