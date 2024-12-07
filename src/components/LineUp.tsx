d5cc6bb50d930c52748be2632e21281e45b61778
sim <>
2024-12-07 13:44:09 +0100

    remove container div and fix minimum height issue

--- src/components/LineUp.tsx
+++ src/components/LineUp.tsx
@@ -11,19 +11,15 @@ import image7 from '../assets/IMG_4377.png';
 
 const LineUp: React.FC = () => {
     return (
-        <div className='md:flex md:items-center md:justify-center md:h-[calc(100vh-300px)]'>
-            <div className='container mx-auto'>
-                <div className='flex flex-col items-center md:flex-row md:justify-between md:items-center'>
-                    <Photo src={image1} alt='Image 1' description='This is Image 1'></Photo>
-                    <Photo src={image2} alt='Image 2' description='This is Image 1'></Photo>
-                    <Photo src={image3} alt='Image 3' description='This is Image 1'></Photo>
-
-                    <Photo src={image4} alt='Image 4' description='This is Image 1'></Photo>
-                    <Photo src={image5} alt='Image 5' description='This is Image 1'></Photo>
-
-                    <Photo src={image6} alt='Image 6' description='This is Image 1'></Photo>
-                    <Photo src={image7} alt='Image 7' description='This is Image 1'></Photo>
-                </div>
+        <div className='md:flex md:items-center md:justify-center md:min-h-[calc(100vh-300px)]'>
+            <div className='flex flex-col items-center md:flex-row md:justify-between md:items-center'>
+                <Photo src={image1} alt='Image 1' description='This is Image 1'></Photo>
+                <Photo src={image2} alt='Image 2' description='This is Image 1'></Photo>
+                <Photo src={image3} alt='Image 3' description='This is Image 1'></Photo>
+                <Photo src={image4} alt='Image 4' description='This is Image 1'></Photo>
+                <Photo src={image5} alt='Image 5' description='This is Image 1'></Photo>
+                <Photo src={image6} alt='Image 6' description='This is Image 1'></Photo>
+                <Photo src={image7} alt='Image 7' description='This is Image 1'></Photo>
             </div>
         </div>
     );
