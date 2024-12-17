13ec0c0af5e29c37a74fed1766c8ad4756a5d7c5
sim <>
2024-12-17 19:57:14 +0100

    active link

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import NavBar from './components/NavBar';
 
 import About from './pages/About';
 import Contact from './pages/Contact';
-import FashionCostumeDesign from './pages/FashionCostumeDesign';
+import Design from './pages/FashionCostumeDesign';
 import Home from './pages/Home';
 import Illustration from './pages/Illustration';
 
@@ -16,11 +16,11 @@ const App: React.FC = () => {
         <Router basename={import.meta.env.BASE_URL}>
             <NavBar />
             <Routes>
-                <Route path='/' element={<Home></Home>}></Route>
-                <Route path='/about' element={<About></About>}></Route>
-                <Route path='/contact' element={<Contact></Contact>}></Route>
-                <Route path='/design' element={<FashionCostumeDesign></FashionCostumeDesign>}></Route>
-                <Route path='/illustration' element={<Illustration></Illustration>}></Route>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/design' element={<Design />} />
+                <Route path='/illustration' element={<Illustration />} />
             </Routes>
         </Router>
     );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 const NavBar: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
@@ -13,9 +13,9 @@ const NavBar: React.FC = () => {
             <nav className='bg-white text-black'>
                 {/* Mobile NavBar*/}
                 <div className='flex md:hidden justify-between items-center p-8'>
-                    <Link to='/'>
+                    <NavLink to='/'>
                         <div className='font-bodoni tracking-wider text-center text-4xl'>Kate Colby</div>
-                    </Link>
+                    </NavLink>
                     <button onClick={toggleMenu} className='focus:outline-none'>
                         <div className='flex flex-col justify-center items-center w-8 h-8'>
                             <span className={`block w-full h-1 bg-black transform transition duration-300 ease-in-out ${isOpen ? 'rotate-45 translate-y-2' : ''}`}></span>
@@ -27,7 +27,9 @@ const NavBar: React.FC = () => {
                 <div className={`fixed inset-0 flex flex-col space-y-8 items-center justify-center bg-[#EEE0CB] z-50 transition-opacity duration-300 ${isOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'}`}>
                     <div className='flex justify-between items-center p-8'>
                         <div className='absolute top-4 left-4 m-4'>
-                            <div className='font-bodoni tracking-wider text-center text-4xl'>Kate Colby</div>
+                            <NavLink to='/' onClick={() => setIsOpen(false)}>
+                                <div className='font-bodoni tracking-wider text-center text-4xl'>Kate Colby</div>
+                            </NavLink>
                         </div>
                         <div className='absolute top-4 right-4 m-4'>
                             <button onClick={toggleMenu} className='text-2xl font-bold'>
@@ -39,52 +41,56 @@ const NavBar: React.FC = () => {
                             </button>
                         </div>
                     </div>
-                    <Link to='/illustration' onClick={toggleMenu} className={`font-bodoni mplus text-3xl mb-4 hover:underline transform transition-transform duration-300 ${isOpen ? '-translate-y-0' : 'translate-y-full'}`}>
+                    <NavLink to='/illustration' onClick={() => setIsOpen(false)} className={({ isActive }) => `font-bodoni text-3xl mb-4 hover:underline ${isActive ? 'underline decoration-black' : ''}`}>
                         Illustration
-                    </Link>
-                    <Link to='/design' onClick={toggleMenu} className={`font-bodoni mplus text-3xl mb-4 hover:underline transform transition-transform duration-300 ${isOpen ? '-translate-y-0' : 'translate-y-full'}`}>
-                        Fasion + Costum Design
-                    </Link>
-                    <Link to='/styling' onClick={toggleMenu} className={`font-bodoni mplus text-3xl mb-4 hover:underline transform transition-transform duration-300 ${isOpen ? '-translate-y-0' : 'translate-y-full'}`}>
+                    </NavLink>
+                    <NavLink to='/design' onClick={() => setIsOpen(false)} className={({ isActive }) => `font-bodoni text-3xl mb-4 hover:underline ${isActive ? 'underline decoration-black' : ''}`}>
+                        Fashion + Costume Design
+                    </NavLink>
+                    <NavLink to='/styling' onClick={() => setIsOpen(false)} className={({ isActive }) => `font-bodoni text-3xl mb-4 hover:underline ${isActive ? 'underline decoration-black' : ''}`}>
                         Styling
-                    </Link>
-                    <Link to='/' onClick={toggleMenu} className={`font-bodoni mplus text-3xl mb-4 hover:underline transform transition-transform duration-300 ${isOpen ? '-translate-y-0' : 'translate-y-full'}`}>
+                    </NavLink>
+                    <NavLink to='/' onClick={() => setIsOpen(false)} className={({ isActive }) => `font-bodoni text-3xl mb-4 hover:underline ${isActive ? 'underline decoration-black' : ''}`}>
                         Home
-                    </Link>
-                    <Link to='/about' onClick={toggleMenu} className={`font-bodoni mplus text-3xl mb-4 hover:underline transform transition-transform duration-300 ${isOpen ? '-translate-y-0' : 'translate-y-full'}`}>
+                    </NavLink>
+                    <NavLink to='/about' onClick={() => setIsOpen(false)} className={({ isActive }) => `font-bodoni text-3xl mb-4 hover:underline ${isActive ? 'underline decoration-black' : ''}`}>
                         About
-                    </Link>
-                    <Link to='/contact' onClick={toggleMenu} className={`font-bodoni mplus text-3xl mb-4 hover:underline transform transition-transform duration-300 ${isOpen ? '-translate-y-0' : 'translate-y-full'}`}>
+                    </NavLink>
+                    <NavLink to='/contact' onClick={() => setIsOpen(false)} className={({ isActive }) => `font-bodoni text-3xl mb-4 hover:underline ${isActive ? 'underline decoration-black' : ''}`}>
                         Contact
-                    </Link>
+                    </NavLink>
                 </div>
 
                 {/* Large Screen Menu*/}
                 <div className='hidden md:block'>
                     <div className='flex justify-start items-center p-3 mx-7 space-x-20 mplus font-normal text-base'>
-                        <Link to='/' className='hover:underline'>
+                        <NavLink to='/' className={({ isActive }) => `hover:underline ${isActive ? 'underline decoration-black' : ''}`}>
                             Home
-                        </Link>
-                        <Link to='/about' className='hover:underline'>
+                        </NavLink>
+                        <NavLink to='/about' className={({ isActive }) => `hover:underline ${isActive ? 'underline decoration-black' : ''}`}>
                             About
-                        </Link>
-                        <Link to='/contact' className='hover:underline'>
-                            contact
-                        </Link>
+                        </NavLink>
+                        <NavLink to='/contact' className={({ isActive }) => `hover:underline ${isActive ? 'underline decoration-black' : ''}`}>
+                            Contact
+                        </NavLink>
                     </div>
-                    <Link to='/'>
-                        <div className='font-bodoni tracking-wider text-center text-6xl'>Kate Colby</div>
-                    </Link>
+
+                    <div className='font-bodoni tracking-wider text-center text-6xl '>
+                        <NavLink to='/'>
+                            <span className='text-black transition-colors duration-300 hover:text-pink-400'>Kate Colby</span>
+                        </NavLink>
+                    </div>
+
                     <div className='flex justify-center items-center p-7 space-x-32 mplus text-xl'>
-                        <Link to='/illustration' className='hover:underline'>
+                        <NavLink to='/illustration' className={({ isActive }) => `hover:underline ${isActive ? 'underline decoration-black' : ''}`}>
                             Illustration
-                        </Link>
-                        <Link to='/design' className='hover:underline'>
+                        </NavLink>
+                        <NavLink to='/design' className={({ isActive }) => `hover:underline ${isActive ? 'underline decoration-black' : ''}`}>
                             Fashion + Costume Design
-                        </Link>
-                        <Link to='/styling' className='hover:underline'>
-                            styling
-                        </Link>
+                        </NavLink>
+                        <NavLink to='/styling' className={({ isActive }) => `hover:underline ${isActive ? 'underline decoration-black' : ''}`}>
+                            Styling
+                        </NavLink>
                     </div>
                 </div>
             </nav>
