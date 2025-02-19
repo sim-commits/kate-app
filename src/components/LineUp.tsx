633b5b9ec4621f12b5b36b04a7b1146ee78f80bc
sim <>
2025-02-19 22:55:07 +0100

    fix double justify between

--- src/components/LineUp.tsx
+++ src/components/LineUp.tsx
@@ -7,7 +7,7 @@ interface LineUpProps {
 
 const LineUp: React.FC<LineUpProps> = ({ images }) => {
     return (
-        <div className='flex flex-col items-center md:flex-row md:justify-between md:justify-between'>
+        <div className='flex flex-col items-center md:flex-row md:justify-between'>
             {images.map((image, index) => (
                 <Photo key={index} src={image.src} alt={image.alt} description={image.description} />
             ))}
