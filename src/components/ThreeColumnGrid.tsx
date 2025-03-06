255d33c0cee2cd328feaba958b062c8a45826549
sim <>
2025-03-06 23:50:58 +0100

    fix ballet path

--- src/components/ThreeColumnGrid.tsx
+++ src/components/ThreeColumnGrid.tsx
@@ -15,8 +15,8 @@ const ThreeColumnGrid = () => {
         { src: image1, link: '/waiwaolani', logo: logo1, subtitle: 'Fashion Collection Development', role: 'Freelance Designer', year: '2025' },
         { src: image2, link: '/david', title: 'David Delfin Contest Submission', subtitle: 'Independent Collection', role: '', year: '2024' },
         { src: image3, link: '/land', logo: logo2, subtitle: 'Li Fung Group', role: 'Fashion Design Intern', year: '2024' },
-        { src: image4, link: '/page3', logo: logo3, title: 'SWAN LAKE', subtitle: 'Seabury Hall', role: 'Studio Assistant', year: '2024' },
-        { src: image5, link: '/ballet', logo: logo3, title: 'THE NUTCRACKER', subtitle: 'Maui Arts & Cultural Center', role: 'Studio Assistant', year: '2025' },
+        { src: image4, link: '/ballet', logo: logo3, title: 'SWAN LAKE', subtitle: 'Seabury Hall', role: 'Studio Assistant', year: '2024' },
+        { src: image5, link: '/nutcrack', logo: logo3, title: 'THE NUTCRACKER', subtitle: 'Maui Arts & Cultural Center', role: 'Studio Assistant', year: '2025' },
         { src: image6, link: '/murder', logo: logo3, title: 'MURDER ON THE ORIENT EXPRESS', subtitle: 'Iao Theater', role: 'Studio Assistant', year: '2025' },
         { src: image7, link: '/jewelry', title: 'Jewelry Design', subtitle: 'Independent Work', year: '2022' },
     ];
